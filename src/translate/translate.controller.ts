88a181590591061e2ff63d05461ad306fac6def0
Thinnakrit <thinnakrit.1996@gmail.com>
2023-05-10 16:09:18 +0700

    update

--- src/translate/translate.controller.ts
+++ src/translate/translate.controller.ts
@@ -4,25 +4,39 @@ import {
   HttpCode,
   Post,
   Query,
+  Req,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { TranslateService } from './translate.service';
 import { TranslateTargetDto, TranslateTextSearchDto } from './translate.dto';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @ApiTags('translate')
 @Controller('translate')
 export class TranslateController {
   constructor(private readonly translateService: TranslateService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Post()
   @HttpCode(200)
   @UsePipes(new ValidationPipe({ transform: true }))
   async translate(
+    @Req() req,
     @Body() body: TranslateTextSearchDto,
     @Query() targetLanguage: TranslateTargetDto,
   ) {
-    return this.translateService.translateText(body, targetLanguage);
+    const translate = await this.translateService.translateText(
+      body,
+      targetLanguage,
+    );
+
+    if (translate) {
+    }
+
+    return translate;
   }
 }
